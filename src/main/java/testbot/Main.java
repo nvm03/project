f861f1bdafcde600b953a2acffa5d3e9abcf154c
nvm0881 <stasriznik150580@gmail.com>
2022-10-05 23:22:37 +0200

    uipu

--- src/main/java/testbot/Main.java
+++ src/main/java/testbot/Main.java
@@ -67,11 +67,11 @@ public class Main {
 	
 	private static Connection getConnection() throws URISyntaxException, SQLException {
 	    
-	    URI dbUri = new URI(System.getenv("jdbc:postgresql://localhost:5432/Contacts_Base?sslmode=require"));
+	    URI dbUri = new URI(System.getenv("jdbc:postgresql://localhost:5432/Contacts_Base?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"));
 
 	    String username = dbUri.getUserInfo().split(":")[0];
 	    String password = dbUri.getUserInfo().split(":")[1];
-	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
 
 	    return DriverManager.getConnection(dbUrl, username, password);
 	}
