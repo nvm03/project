38193d2cba4229ff6a117d08d5f289a046e82973
nvm0881 <stasriznik150580@gmail.com>
2022-10-08 01:58:35 +0200

    upd

--- Contacts.sql
+++ Contacts.sql
@@ -9,3 +9,4 @@ CREATE TABLE contacts(
 INSERT into contacts(Name, Phone_Number, Telegram_UserName) values('Sergiy', '11111111', '@invmm');
 UPDATE contacts set Name = 'Sergiy Fatkula213y' where id = 1;
 DELETE from contacts where id = 1;
+DROP TABLE contacts;
